edb3b8ee9f441d61e731d1658c0de311af7df6da
Haris-bin-Rahat <81488844+Rage-coder69@users.noreply.github.com>
2022-12-18 17:11:08 +0500

    working on order/product relation

--- bechoBook/database/migrations/2022_12_16_180426_create_orders_table.php
+++ bechoBook/database/migrations/2022_12_16_180426_create_orders_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->string('state');
             $table->string('pincode');
             $table->string('phone_number');
-            $table->foreignId('product_id')->constrained("products");
             $table->string('order_status');
             $table->timestamps();
         });
